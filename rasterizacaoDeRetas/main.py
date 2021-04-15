c90003e858e292f2a4ab10e24246eedc09f581b4
João Marcus <joaomarcus12769@gmail.com>
2021-04-15 13:54:09 -0300

    deixando o código mais limpo

--- rasterizacaoDeRetas/main.py
+++ rasterizacaoDeRetas/main.py
@@ -16,9 +16,11 @@ Definindo as retas padrões
 # Reta A
 PONTO_ORIGEM_A = (0, 0)
 PONTO_DESTINO_A = (1, 1)
+
 # Reta B
 PONTO_ORIGEM_B = (0.2, 0.4)
 PONTO_DESTINO_B = (0.6, 0.7)
+
 # Reta C
 PONTO_ORIGEM_C = (0.3, 0.9)
 PONTO_DESTINO_C = (0.2, 0.3)
@@ -31,10 +33,9 @@ def produz_fragmento(x: float, y: float) -> (float, float):
     :param y: componente y do ponto
     :return x_pixel, y_pixel: Posição do centro do pixel
     """
-    x_chao = math.floor(x)
-    y_chao = math.floor(y)
-    x_pixel = x_chao + 0.5
-    y_pixel = y_chao + 0.5
+    x_pixel = math.floor(x) + 0.5
+    y_pixel = math.floor(y) + 0.5
+    
     return x_pixel, y_pixel
 
 
@@ -58,46 +59,56 @@ def rasterizacao_reta(
     x_destino_normalizado = ponto_destino[0]
     y_destino_normalizado = ponto_destino[1]
 
+    # Transformar os pontos da coordenada normalizada na devida resolução
     x_origem = x_origem_normalizado * numero_de_colunas
     y_origem = y_origem_normalizado * numero_de_linhas
 
     x_destino = x_destino_normalizado * numero_de_colunas
     y_destino = y_destino_normalizado * numero_de_linhas
 
+    # calculo da variação no eixo x e y
     delta_x = x_destino - x_origem
     delta_y = y_destino - y_origem
 
+    # Considerando a equação da reta como y = a*x + b
     coeficiente_angular = 0 if delta_x == 0 else delta_y / delta_x
     coeficiente_linear = y_destino - coeficiente_angular * x_destino
 
-    lista_dos_pixels = list()
+    # Guarda os pixels que devem ser preenchidos
+    pixels = list()
 
     x = x_origem
     y = y_origem
 
-    lista_dos_pixels.append(produz_fragmento(x, y))
+    # Guarda o primeiro pixel da reta rasterizada
+    pixels.append(produz_fragmento(x, y))
 
+    # Caso a variação no eixo x seja maior do que a variação no eixo y,
+    # o algoritmo irá iterar x até passar do x_destino, y é calculado pela equação da reta.
+    # Caso contrário, a iteração ocorre no eixo y.
     if abs(delta_x) > abs(delta_y):
+        # Caso a x_origem seja maior, vamos trocá-la pelo x_destino
         if x_origem > x_destino:
             x = x_destino
             x_origem, x_destino = x_destino, x_origem
         while x < x_destino:
             x = x + 1
             if x >= x_destino:
-                return lista_dos_pixels
+                return pixels
             y = coeficiente_angular * x + coeficiente_linear
-            lista_dos_pixels.append(produz_fragmento(x, y))
+            pixels.append(produz_fragmento(x, y))
     else:
+        # Caso a y_origem seja maior, vamos trocá-la pelo y_destino
         if y_origem > y_destino:
             y = y_destino
             y_origem, y_destino = y_destino, y_origem
         while y < y_destino:
             y = y + 1
             if y >= y_destino:
-                return lista_dos_pixels
+                return pixels
             x = x if coeficiente_angular == 0 else (y - coeficiente_linear) / coeficiente_angular
-            lista_dos_pixels.append(produz_fragmento(x, y))
-    return lista_dos_pixels
+            pixels.append(produz_fragmento(x, y))
+    return pixels
 
 
 def produz_matriz(
@@ -131,10 +142,11 @@ def cria_imagem(
     :param resolucao: Resolucao utilizada
     :return: None
     """
-    plt.figure()
-    plt.title(nome_da_imagem)
     reta_rasterizada = rasterizacao_reta(ponto_origem, ponto_destino, resolucao)
     matriz = produz_matriz(reta_rasterizada, resolucao)
+    
+    plt.figure()
+    plt.title(nome_da_imagem)    
     img = plt.imshow(matriz, cmap='Greys', origin='lower')
     plt.savefig("rasterizacaoDeRetas/" + nome_da_imagem)
     plt.show()
