20fa5448a4af588136a0342850f4696953226b34
João Marcus <joaomarcus12769@gmail.com>
2021-04-17 16:04:59 -0300

    refatorando

--- rasterizacaoDeRetas/main.py
+++ rasterizacaoDeRetas/main.py
@@ -3,24 +3,24 @@ import matplotlib.pyplot as plt
 from typing import List, Tuple
 
 """
-Definindo as resoluções padrões
+Definindo as resoluções padrões utilizadas
 """
 RESOLUCAO_40_30 = (40, 30)
 RESOLUCAO_80_60 = (80, 60)
 RESOLUCAO_QVGA = (320, 240)
 
 """
-Definindo as retas padrões
+Definindo as retas padrões, os pontos pertencem ao SCN
 """
-# Reta A
+# Reta A - Variação maior no eixo x
 PONTO_ORIGEM_A = (0.2, 0.3)
 PONTO_DESTINO_A = (0.9, 0.5)
 
-# Reta B
+# Reta B - Variação maior no eixo y
 PONTO_ORIGEM_B = (0.3, 0.2)
 PONTO_DESTINO_B = (0.5, 0.9)
 
-# Reta C
+# Reta C - Variação maior no eixo y
 PONTO_ORIGEM_C = (0.3, 0.9)
 PONTO_DESTINO_C = (0.2, 0.3)
 
@@ -40,6 +40,18 @@ PONTO_DESTINO_F = (0.75, 0.24)
 PONTO_ORIGEM_G = (0.25, 0.25)
 PONTO_DESTINO_G = (0.24, 0.75)
 
+# Reta H - Horizontal Borda Superior
+PONTO_ORIGEM_H = (0, 1)
+PONTO_DESTINO_H = (1, 1)
+
+# Reta I - Vertical Borda Direita
+PONTO_ORIGEM_I = (1, 0)
+PONTO_DESTINO_I = (1, 1)
+
+# Reta J - Diagonal secundária
+PONTO_ORIGEM_J = (0, 1)
+PONTO_DESTINO_J = (1, 0)
+
 
 def produz_fragmento(x: float, y: float) -> (float, float):
     """
@@ -50,14 +62,14 @@ def produz_fragmento(x: float, y: float) -> (float, float):
     """
     x_pixel = math.floor(x) + 0.5
     y_pixel = math.floor(y) + 0.5
-    
+
     return x_pixel, y_pixel
 
 
 def rasterizacao_reta(
         ponto_origem: Tuple[float, float],
         ponto_destino: Tuple[float, float],
-        resolucao: (int, int)) -> List[Tuple[float, float]]:
+        resolucao: Tuple[int, int]) -> List[Tuple[float, float]]:
     """
     Algoritmo de rasterização de retas
     :param ponto_origem: Entre 0 e 1, representam o ponto de origem da reta
@@ -101,36 +113,58 @@ def rasterizacao_reta(
     x = x_origem
     y = y_origem
 
-    # Guarda o primeiro pixel da reta rasterizada
-    pixels.append(produz_fragmento(x, y))
-
     # Caso a variação no eixo x seja maior do que a variação no eixo y,
     # o algoritmo irá iterar x até passar do x_destino, y é calculado pela equação da reta.
     # Caso contrário, a iteração ocorre no eixo y.
     if abs(delta_x) > abs(delta_y):
-        # Caso a x_origem seja maior, vamos trocá-la pelo x_destino
-        if x_origem > x_destino:
-            x = x_destino
+        if delta_x < 0:
+            x, y = x_destino, y_destino
             x_origem, x_destino = x_destino, x_origem
-        while x < x_destino:
-            x = x + 1
-            if x >= x_destino:
-                return pixels
-            y = coeficiente_angular * x + coeficiente_linear
-            pixels.append(produz_fragmento(x, y))
-    else:
-        # Caso a y_origem seja maior, vamos trocá-la pelo y_destino
-        if y_origem > y_destino:
-            y = y_destino
             y_origem, y_destino = y_destino, y_origem
-        while y < y_destino:
-            y = y + 1
-            if y >= y_destino:
-                return pixels
-            # x é atualizado apenas quando delta_x difere de 0
-            if delta_x != 0:
-                x = (y - coeficiente_linear) / coeficiente_angular
+        while x < x_destino:
+            # tratamento da borda
+            if x == numero_de_colunas:
+                x = x - 0.5
+
+            # tratamento da borda
+            if y == numero_de_linhas:
+                y = y - 0.5
+
             pixels.append(produz_fragmento(x, y))
+            x = x + 1
+            y = coeficiente_angular * x + coeficiente_linear
+    else:
+        if delta_y < 0:
+            x, y = x_destino, y_destino
+            x_origem, x_destino = x_destino, x_origem
+            y_origem, y_destino = y_destino, y_origem
+
+        if coeficiente_angular is None:
+            while y < y_destino:
+                # tratamento da borda
+                if x == numero_de_colunas:
+                    x = x - 0.5
+
+                # tratamento da borda
+                if y == numero_de_linhas:
+                    y = y - 0.5
+
+                pixels.append(produz_fragmento(x, y))
+                y = y + 1
+                x = x
+            return pixels
+        while y < y_destino:
+            # tratamento da borda
+            if x == numero_de_colunas:
+                x = x - 0.5
+
+            # tratamento da borda
+            if y == numero_de_linhas:
+                y = y - 0.5
+
+            pixels.append(produz_fragmento(x, y))
+            y = y + 1
+            x = (y - coeficiente_linear) / coeficiente_angular
     return pixels
 
 
@@ -143,13 +177,14 @@ def produz_matriz(
     :param resolucao: resolução utilizada na rasterização
     :return: produz uma matrix com as marcações da reta rasterizada, a matriz tem o tamanho da resolução
     """
+    # Produz matriz preechida de zeros
     matriz = [[0 for coluna in range(resolucao[0])] for linha in range(resolucao[1])]
 
     # Devemos marcar na matriz os pontos existentes no array rasterização
     for pixel in array_rasterizacao:
-        linha = math.floor(pixel[0])
-        coluna = math.floor(pixel[1])
-        matriz[coluna][linha] = 1
+        coluna = math.floor(pixel[0])
+        linha = math.floor(pixel[1])
+        matriz[linha][coluna] = 1
     return matriz
 
 
@@ -168,9 +203,9 @@ def cria_imagem(
     """
     reta_rasterizada = rasterizacao_reta(ponto_origem, ponto_destino, resolucao)
     matriz = produz_matriz(reta_rasterizada, resolucao)
-    
+
     plt.figure()
-    plt.title(nome_da_imagem)    
+    plt.title(nome_da_imagem)
     img = plt.imshow(matriz, cmap='Greys', origin='lower')
     plt.savefig(nome_da_imagem)
     plt.show()
@@ -210,3 +245,16 @@ cria_imagem(PONTO_ORIGEM_F, PONTO_DESTINO_F, RESOLUCAO_QVGA, "Reta F Na resoluç
 cria_imagem(PONTO_ORIGEM_G, PONTO_DESTINO_G, RESOLUCAO_40_30, "Reta G na resolução: 40x30")
 cria_imagem(PONTO_ORIGEM_G, PONTO_DESTINO_G, RESOLUCAO_80_60, "Reta G na resolução: 80x60")
 cria_imagem(PONTO_ORIGEM_G, PONTO_DESTINO_G, RESOLUCAO_QVGA, "Reta G Na resolução: 320x240 (QVGA)")
+
+# Gerando Reta H
+cria_imagem(PONTO_ORIGEM_H, PONTO_DESTINO_H, RESOLUCAO_40_30, "Reta H na resolução: 40x30")
+cria_imagem(PONTO_ORIGEM_H, PONTO_DESTINO_H, RESOLUCAO_80_60, "Reta H na resolução: 80x60")
+
+# Gerando Reta I
+cria_imagem(PONTO_ORIGEM_I, PONTO_DESTINO_I, RESOLUCAO_40_30, "Reta I na resolução: 40x30")
+cria_imagem(PONTO_ORIGEM_I, PONTO_DESTINO_I, RESOLUCAO_80_60, "Reta I na resolução: 80x60")
+
+# Gerando Reta J
+cria_imagem(PONTO_ORIGEM_J, PONTO_DESTINO_J, RESOLUCAO_40_30, "Reta J na resolução: 40x30")
+cria_imagem(PONTO_ORIGEM_J, PONTO_DESTINO_J, RESOLUCAO_80_60, "Reta J na resolução: 80x60")
+cria_imagem(PONTO_ORIGEM_J, PONTO_DESTINO_J, RESOLUCAO_QVGA, "Reta J Na resolução: 320x240 (QVGA)")
