4644d1db384d97334b7dab1b0e3e861948f7d26e
João Marcus <joaomarcus12769@gmail.com>
2021-06-16 19:40:56 -0300

    add trab 2

---
+++ trabalho2/questao2.py
@@ -0,0 +1,15 @@
+# 1) Componha uma cena contendo os diversos sólidos modelados anteriormente
+# em um sistema de coordenadas do mundo, de tal maneira a não haver
+# sobreposição ou intersecção entre tais objetos.
+#
+# a. O cubo e a pirâmide devem estar localizados em apenas um octante do
+# espaço, bem como o paralelepípedo e o tronco devem estar em apenas
+# um octante. Além disso, pelo menos dois octantes adjacentes devem
+# possuir sólidos.
+#
+# b. O maior valor possível para cada uma das componentes de um vértice é
+# 6. Se necessário aplique transformações de escala para que os sólidos
+# sejam localizados respeitando tais limites.
+#
+# c. Apresente os diversos sólidos neste sistema de coordenadas em 3D.
+
---
+++ trabalho2/questao3.py
@@ -0,0 +1,13 @@
+# 3) Escolha um dos octantes sem nenhum sólido e escolha um ponto como origem
+# para o sistema de coordenadas da câmera.
+#
+# a. Compute a base vetorial do novo sistema de coordenadas. Para isso,
+# tenha como base apenas um dos octantes que será considerado como o
+# volume de visão e use o ponto médio entre os centros de massa de cada
+# um dos sólidos na derivação de tal base vetorial.
+#
+# b. Transforme os objetos do sistema de coordenadas do mundo para o
+# sistema de coordenadas da câmera.
+#
+# c. Apresente os diversos sólidos neste sistema de coordenadas em 3D.
+
