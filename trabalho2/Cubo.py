2d86ffff69d9d8a55ee24da9444ad049d0f86f51
João Marcus <joaomarcus12769@gmail.com>
2021-06-17 23:39:38 -0300

    tirando faces de Cubo.PY

--- trabalho2/Cubo.py
+++ trabalho2/Cubo.py
@@ -6,7 +6,6 @@ class Cubo:
 
         self.set_vertices(tamanho_aresta)
         self.set_arestas({})
-        self.set_faces({})
 
     @property
     def vertices(self):
@@ -15,10 +14,6 @@ class Cubo:
     @property
     def arestas(self):
         return self._arestas
-
-    @property
-    def faces(self):
-        return self._faces
 
     def set_vertices(self, tamanho_aresta: float):
         self._vertices["V1"] = (0, 0, 0)
@@ -43,11 +38,3 @@ class Cubo:
         self._arestas["A10"] = ("V5", "V8")
         self._arestas["A11"] = ("V6", "V8")
         self._arestas["A12"] = ("V7", "V8")
-
-    def set_faces(self, faces: dict):
-        self._faces["F1"] = ("A1", "A1", "A1", "A1")
-        self._faces["F2"] = ("A1", "A1", "A1", "A1")
-        self._faces["F3"] = ("A1", "A1", "A1", "A1")
-        self._faces["F4"] = ("A1", "A1", "A1", "A1")
-        self._faces["F5"] = ("A1", "A1", "A1", "A1")
-        self._faces["F6"] = ("A1", "A1", "A1", "A1")
