a61ee48f14d3db5804d108dbf049d8bb1c6e1c9d
João Marcus <joaomarcus12769@gmail.com>
2021-06-24 17:31:34 -0300

    adicionando novos comentários no sólido e refatorando

--- trabalho2/src/Solido.py
+++ trabalho2/src/Solido.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from typing import List
 
 import numpy as np
+from numpy import ndarray
 
 
 @dataclass
@@ -17,30 +18,51 @@ class Solido:
         self.titulo = titulo
         self.cor = cor
 
-    def array_de_vertices(self):
+    def array_de_vertices(self) -> ndarray:
+        """
+        :return: Array contendo cada vértice como [x, y, z]
+        """
         return np.array(list(self.vertices.values()))
 
-    def matriz_para_vertices(self, matriz: np.array) -> None:
+    def matriz_para_vertices(self, array_vertice: ndarray) -> None:
+        """
+        Recebe um array contendo vértices [x, y, z] e substitui os vértices do sólidos pelos do array.
+        :param array_vertice: Array de vertices [x, y, z]
+        :return: None
+        """
         for i, vertice in enumerate(self.vertices):
-            self.vertices[vertice] = matriz[i]
+            self.vertices[vertice] = array_vertice[i]
 
-    def multiplicacao_por_matriz(self, matriz: np.array) -> None:
-        l = []
+    def multiplicacao_por_matriz(self, matriz: np.matrix) -> None:
+        """
+        Para cada vértice V [x, y, z] do sólido, o colocamos em sua forma homogênea [x, y, z, 1]. Seja M uma matriz,
+        multiplicamos cada vértice pela matriz M * V1, sendo V1 um vértice do sólido em sua forma homogênea.
+        :param matriz: Matriz a ser multiplicada
+        :return: None
+        """
+        uns = [[1.0]] * len(self.vertices)  # array de uns [1.0, 1.0, ...]
+        x = np.append(self.array_de_vertices(), uns, axis=1)
+        matriz_vertices_homogenea = np.asmatrix(x)
+        matriz_transposta_vertices = matriz_vertices_homogenea.transpose()
+
+        # Multiplicação da matriz pra cada vértice
         for i, vertice in enumerate(self.vertices):
-            l.append([1.0])
-
-        x = np.append(self.array_de_vertices(), l, axis=1)
-        m = np.asmatrix(x)
-        matriz_transposta = m.transpose()
-
-        for i, vertice in enumerate(self.vertices):
-            col = matriz_transposta[:, i]
+            col = matriz_transposta_vertices[:, i]
             nova_col = matriz * col
-            matriz_transposta[:, i] = nova_col
+            matriz_transposta_vertices[:, i] = nova_col
 
-        self.matriz_para_vertices(np.array(matriz_transposta.transpose()))
+        matriz_transposta_vertices = np.delete(matriz_transposta_vertices, -1, 0)  # Remove a linha de uns
+        self.matriz_para_vertices(np.array(matriz_transposta_vertices.transpose()))
 
     def adiciona_nos_eixos(self, eixo_x: float = 0, eixo_y: float = 0, eixo_z: float = 0) -> None:
+        """
+        Equivalente de utilizar uma matriz de translação. Recebe-se o valor dos eixos x, y e z e adiciona-os
+        nos seus respectivos eixos.
+        :param eixo_x: Valor a adicionar no eixo x
+        :param eixo_y: Valor a adicionar no eixo y
+        :param eixo_z: Valor a adicionar no eixo z
+        :return: None
+        """
         matriz_vertices = self.array_de_vertices()
         matriz_vertices[:, [0]] = matriz_vertices[:, [0]] + eixo_x
         matriz_vertices[:, [1]] = matriz_vertices[:, [1]] + eixo_y
